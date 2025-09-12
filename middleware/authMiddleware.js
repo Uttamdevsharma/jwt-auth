aa3afa4483b2f872f7676cea456b26027dd3ce3e
Uttam <uttamrohit4545@gmail.com>
2025-09-13 02:53:43 +0600

    feat : succesfully verify token and access route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,19 @@ const jwt = require("jsonwebtoken")
 
 const verifyToken = (req,res,next) => {
 
+    const authHeaders = req.headers.authorization;
+    const token = authHeaders && req.headers.authorization.split(' ')[1]
     
+    if(!token) return res.send({message : "Access Denied , Unauthorized token"  })
+
+    jwt.verify(token , process.env.JWT_SECRET_KEY , (err, user) => {
+        if(err) return res.send({message : "Invalid Token"})
+
+            req.user = user
+            next()    
+    })    
+
+   
 
 }
 
